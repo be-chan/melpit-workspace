4b22fd6f50e070f120c98428760e91d3c68b6772
be-chan <70454487+be-chan@users.noreply.github.com>
2022-01-19 00:24:38 +0900

    一旦commit

--- melpit/routes/web.php
+++ melpit/routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('top');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('mypage')
+    ->namespace('MyPage')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('edit-profile', 'ProfileController@showProfileEditForm')->name('mypage.edit-profile');
+        Route::post('edit-profile','ProfileController@editProfile')->name('mypage.edit-profile');
+    });
