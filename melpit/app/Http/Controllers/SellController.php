2d372b3b080099a0498cd7dfbcbbf04569c98367
be-chan <70454487+be-chan@users.noreply.github.com>
2022-01-23 23:35:41 +0900

    PAYJPの設定まで

--- melpit/app/Http/Controllers/SellController.php
+++ melpit/app/Http/Controllers/SellController.php
@@ -7,7 +7,11 @@ use App\Http\Requests\SellRequest;
 use App\Models\Item;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class SellController extends Controller
 {
@@ -32,18 +36,57 @@ class SellController extends Controller
     public function sellItem(SellRequest $request)
     {
         $user = Auth::user();
+        $imageName = $this->saveImage($request->file('item-image'));
 
-        $item = new Item();
+        $item = new Item(); //Itemモデルのインスタンス化
+
+        //ここから下は左がカラム名、右側がRequestでbladeのname属性から値を取得
+        // つまり、左のカラムに対して、右側のRequestが入力した値を代入
+        $item->image_file_name = $imageName;
         $item->seller_id = $user->id;
+
         $item->name = $request->input('name');
+        // $item->name = $request->name; //この書き方でも可
+        
         $item->description = $request->input('description');
         $item->secondary_category_id = $request->input('category');
         $item->item_condition_id = $request->input('condition');
         $item->price = $request->input('price');
         $item->state = Item::STATE_SELLING;
-        $item->save();
+        $item->save(); //$itemに入ったデータを保存。
 
         return redirect()->back()
         ->with('status', '商品を出品しました。');
     }
+
+    /**
+      * 商品画像をリサイズして保存します
+      *
+      * @param UploadedFile $file アップロードされた商品画像
+      * @return string ファイル名
+      */
+
+    private function saveImage(UploadedFile $file):string
+    {
+        $tempPath = $this->makeTempPath();
+
+        Image::make($file)->fit(300, 300)->save($tempPath);
+
+        $filePath = Storage::disk('public')
+        ->putFile('item-images', new File($tempPath));
+
+        return basename($filePath);
+    }
+
+    /**
+      * 一時的なファイルを生成してパスを返します。
+      *
+      * @return string ファイルパス
+      */
+      private function makeTempPath(): string
+      {
+          $tmp_fp = tmpfile();
+          $meta   = stream_get_meta_data($tmp_fp);
+          return $meta["uri"];
+      }
 }
